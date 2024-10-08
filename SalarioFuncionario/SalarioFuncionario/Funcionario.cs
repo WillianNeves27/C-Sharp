0bfc81e44badd1652949498fbc372462686ac129
WillianNeves27 <150395917+WillianNeves27@users.noreply.github.com>
2024-10-07 21:27:07 -0300

    Add files via upload
    Sistema com a funcionalidade de fazer calculo de valor liquido do salário de um funcionario e calculo de aumento de salário.

---
+++ SalarioFuncionario/SalarioFuncionario/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarioFuncionario
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentoSalario(double Porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * Porcentagem / 100);
+        }
+
+        public string SaidaDado()
+        {
+            return this.Nome + ", " + "R$" + this.SalarioLiquido().ToString("N2");
+        }
+    }
+}
---
+++ SalarioFuncionario/SalarioFuncionario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace SalarioFuncionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salario: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+            Console.WriteLine("Funcionário: " + funcionario.SaidaDado());
+            Console.WriteLine("-------------------------------");
+            Console.Write("Aumento de Salário (Porcentagem): ");
+            double Porcentagem = double.Parse(Console.ReadLine());
+            funcionario.AumentoSalario(Porcentagem);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Dados Atualizados: " + funcionario.SaidaDado());
+        }
+    }
+}
