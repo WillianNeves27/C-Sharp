0c51d73a68bec1f9c620521aa18669e61efa0526
WillianNeves27 <150395917+WillianNeves27@users.noreply.github.com>
2024-10-07 21:17:00 -0300

    Add files via upload
    Sistema em C# para somatória de notas onde o mesmo calcula se o aluno ficou reprovado ou se foi aprovado.

---
+++ Boletim/Boletim/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Boletim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SomatoriaNotas nota = new SomatoriaNotas();
+            Console.Write("Nome do Aluno: ");
+            nota.nome = Console.ReadLine();
+            Console.Write("Nota 1º trimestre: ");
+            nota.nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 2º trimestre: ");
+            nota.nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Nota 3º trimestre: ");
+            nota.nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Nota Final = " + nota.somatoriaFinal());
+
+            if (nota.somatoriaFinal() > nota.media)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!");
+                Console.WriteLine("Faltaram " + nota.diferencaPontos().ToString("F2") + " pontos");
+            }
+        }
+    }
+}
---
+++ Boletim/Boletim/SomatoriaNotas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletim
+{
+    internal class SomatoriaNotas
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+        public double media = 60.00;
+
+        public double somatoriaFinal()
+        {
+            return nota1 + nota2 + nota3;
+        }
+
+        public double diferencaPontos()
+        {
+            return media - somatoriaFinal();
+        }
+    }
+}
